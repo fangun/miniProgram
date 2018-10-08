e978f1efe4999a473b6f06dbde85e634b7b294ee
Unknown <nry123@qq.com>
2018-10-08 19:03:21 +0800

    1008

--- pages/storeHead/index.js
+++ pages/storeHead/index.js
@@ -9,7 +9,11 @@ Page({
     storeIntro:"介绍介绍介绍介绍介绍介绍介绍介绍介绍介介绍介绍介绍介绍介绍介绍介绍介绍介绍介",
     storeLocation:"上海市xx区xx路224",
     storeTel:"00000000000",
-    storeImage:"https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=2338693105,1140444294&fm=58&bpow=512&bpoh=512"
+    storeImage:"https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=2338693105,1140444294&fm=58&bpow=512&bpoh=512",
+
+    blockArray:["选择服务项目","选择服务人员","选择服务时间"],
+    seviceProjects:["刺青","清洗纹身","疤痕遮盖","刺青","清洗纹身","呵呵呵"]
+
   },
 
   /**
