65bc7e48f437da6c9a0801eaec83029e80daa325
ydlx <lichong536@163.com>
2018-10-09 18:20:15 +0800

    shouye

--- pages/customEntrance/index.js
+++ pages/customEntrance/index.js
@@ -6,46 +6,88 @@ Page({
   data: {
 
     pageTitle: '预约吧',
-    hotlistTitle:'我的预约热榜',
-    diaryTitle:'我的预约日记',
+    hotlistTitle: {
+      'c': '我的预约热榜',
+      'e': 'Appointment Trending'
+    },
+    diaryTitle: {
+      'c': '我的预约日记',
+      'e': 'Appointment Book'
+    },
 
-    hostListData:[{'src':'../../resource/images/customEntrance/test.png','text':'11'}, 
-    {'src':'../../resource/images/customEntrance/test.png','text':'22'}, 
-    {'src':'../../resource/images/customEntrance/test.png','text':'33'},
-    {'src':'../../resource/images/customEntrance/test.png','text':'44'}, 
-    {'src':'../../resource/images/customEntrance/test.png','text':'55'}, 
-    {'src':'../../resource/images/customEntrance/test.png','text':'66'},
-    {'src':'../../resource/images/customEntrance/test.png','text':'77'},
-    {'src':'../../resource/images/customEntrance/test.png','text':'88'}]
+    hostListData: [{
+      'src': '../../resource/images/customEntrance/05.png',
+      'text': '11'
+    }, {
+      'src': '../../resource/images/customEntrance/01.png',
+      'text': '22'
+    }, {
+      'src': '../../resource/images/customEntrance/02.png',
+      'text': '33'
+    }, {
+      'src': '../../resource/images/customEntrance/03.png',
+      'text': '44'
+    }, {
+      'src': '../../resource/images/customEntrance/04.png',
+      'text': '55'
+    }, {
+      'src': '../../resource/images/customEntrance/05.png',
+      'text': '66'
+    }, {
+      'src': '../../resource/images/customEntrance/01.png',
+      'text': '77'
+    }, {
+      'src': '../../resource/images/customEntrance/02.png',
+      'text': '88'
+    }],
+
+    tabState: 'doing',
+    tabContentShow: 0
   },
- 
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+
+  selectDoing: function(e) {
+    this.setData({
+      tabState: e.currentTarget.dataset.id,
+      tabContentShow: 0
+    })
+  },
+
+  selectDone: function(e) {
+    this.setData({
+      tabState: e.currentTarget.dataset.id,
+      tabContentShow: '-100%'
+    })
+  },
+
+  onLoad: function() {
+
+    // if (app.globalData.userInfo) {
+    //   this.setData({
+    //     userInfo: app.globalData.userInfo,
+    //     hasUserInfo: true
+    //   })
+    // } else if (this.data.canIUse) {
+    //   // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+    //   // 所以此处加入 callback 以防止这种情况
+    //   app.userInfoReadyCallback = res => {
+    //     this.setData({
+    //       userInfo: res.userInfo,
+    //       hasUserInfo: true
+    //     })
+    //   }
+    // } else {
+    //   // 在没有 open-type=getUserInfo 版本的兼容处理
+    //   wx.getUserInfo({
+    //     success: res => {
+    //       app.globalData.userInfo = res.userInfo
+    //       this.setData({
+    //         userInfo: res.userInfo,
+    //         hasUserInfo: true
+    //       })
+    //     }
+    //   })
+    // }
+    
   }
 
 })
