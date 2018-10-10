2698b93cd26826913ee8299289934df2bc749e01
ydlx <lichong536@163.com>
2018-10-10 18:47:36 +0800

    tabar

---
+++ pages/addAppointment/index.js
@@ -0,0 +1,27 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    pageTitle: '预约吧',
+
+  },
+
+  customEntrance: function() {
+    wx.navigateTo({
+      url: `../customEntrance/index`
+    })
+  },
+  
+  storeEntrance: function() {
+    wx.navigateTo({
+      url: `../storeHead/index`
+    })
+  },
+
+  onLoad: function () {
+
+  }
+
+})
