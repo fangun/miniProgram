2b3777e73e4f88ce309d4f10b7ec5114b3464cd3
ydlx <lichong536@163.com>
2018-10-27 18:22:12 +0800

    预约吧上线第一版

--- pages/feedback/index.js
+++ pages/feedback/index.js
@@ -7,39 +7,58 @@ Page({
     pageTitle: '意见反馈'
   },
 
-  feedback: function (e) {
-
+  feedback: function(e) {
     var Content = e.detail.value.Content;
     var Contact = e.detail.value.Contact;
+    if (!Content || Content == '') {
+          wx.showToast({
+            title: '请填写反馈内容',
+            icon:'none',
+            success: function() {
+            }
+          });
+    } else if (!Contact || Contact == '') {
+          wx.showToast({
+            title: '请填写联系方式',
+            icon:'none',
+            success: function() {
+            }
+          });
+    } else {
+      wx.request({
+        url: 'https://api.yuyue58.cn/api/ProblemFeedback',
+        method: "POST",
+        data: {
+          Page: 'pages/feedback/index',
+          Content: Content,
+          Contact: Contact,
+          mid: app.globalData.peopleInfo.mid,
+          title: '用户反馈'
+        },
+        header: {
+          "content-type": "application/x-www-form-urlencoded"
+        },
+        success(res) {
+          wx.showToast({
+            title: '提交成功',
+            success: function() {
+              setTimeout(function() {
+                // 返回上一页
+                var pageInn = getCurrentPages();
+                wx.navigateBack({
+                  delta: pageInn.length - 1
+                })
+              }, 1600);
+            }
+          });
+        }
+      });
+    }
 
-    wx.request({
-      url: 'https://api.yuyue58.cn/api/ProblemFeedback',
-      method: "POST",
-      data: {
-        Page: 'pages/feedback/index',
-        Content: Content,
-        Contact: Contact,
-        mid: app.globalData.peopleInfo.mid,
-        title: '用户反馈'
-      },
-      header: { "content-type": "application/x-www-form-urlencoded" },
-      success(res) {
-        wx.showToast({
-          title: '提交成功',
-          success: function () {
-            setTimeout(function () {
-              // 返回上一页
-              var pageInn = getCurrentPages();
-              wx.navigateBack({
-                delta: pageInn.length - 1
-              })
-            }, 1600);
-          }
-        });
-      }
-    });
+
+
   },
 
-  onLoad: function () { }
+  onLoad: function() {}
 
 })
